ed3bebd0277db63b782ca4e82d6d5db5f89ca533
Luciano Luiz Romero <40298952+lucianoromero@users.noreply.github.com>
2020-09-15 12:17:51 -0300

    Foreach 02

--- src/lambdas/Foreach.java
+++ src/lambdas/Foreach.java
@@ -6,20 +6,30 @@ import java.util.List;
 public class Foreach {
 
 	public static void main(String[] args) {
-		
-		List<String> aprovados = Arrays.asList("Ana","Bia","Lia","Gui");
-		
+
+		List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia", "Gui");
+
 		System.out.println("Forma tradicional...");
-		for(String nome: aprovados) {
-		System.out.println(nome);	
+		for (String nome : aprovados) {
+			System.out.println(nome);
 		}
 
 		System.out.println("\nLambda #01...");
-		aprovados.forEach((nome) ->  System.out.println(nome +"!!!"));
+		aprovados.forEach((nome) -> System.out.println(nome + "!!!"));
 		
-		System.out.println("\nMethod Reference...");
+		System.out.println("\nLambda #02...");
+		aprovados.forEach((nome) -> meuImprimir(nome));
+
+		System.out.println("\nMethod Reference #01...");
 		aprovados.forEach(System.out::println);
 		
+		System.out.println("\nMethod Reference #02...");
+		aprovados.forEach(Foreach::meuImprimir);
+
+	}
+
+	static void meuImprimir(String nome) {
+		System.out.println("Oi! Meu nome é " + nome);
 	}
 
 }
