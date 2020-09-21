44a47ce5c96c80d3ca3aa398c5af994a836f8cb1
Luciano Luiz Romero <40298952+lucianoromero@users.noreply.github.com>
2020-09-21 17:11:07 -0300

    Consumer
    Não retorna nada

--- src/lambdas/Produto.java
+++ src/lambdas/Produto.java
@@ -1,17 +1,21 @@
 package lambdas;
 
 public class Produto extends Object {
-	
+
 	final String nome;
 	final double preco;
 	final double desconto;
-	
+
 	public Produto(String nome, double preco, double desconto) {
 		this.nome = nome;
 		this.preco = preco;
 		this.desconto = desconto;
 	}
-	
-	
+
+	// funcao toString
+	public String toString() {
+		double precoFinal = preco * (1 - desconto);
+		return "Nome: " + nome + "tem preco de: " + precoFinal;
+	}
 
 }
