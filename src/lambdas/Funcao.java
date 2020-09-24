b6b5c8c6118126a849545b957059a97c9049ad5a
Luciano Luiz Romero <40298952+lucianoromero@users.noreply.github.com>
2020-09-24 18:38:53 -0300

    PredicadoComposicao

--- src/lambdas/Funcao.java
+++ src/lambdas/Funcao.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public class Funcao {
 
 	public static void main(String[] args) {
-		/// Basica 
+
 		Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "Par" : "Impar";
 
 		System.out.println(parOuImpar.apply(33));
